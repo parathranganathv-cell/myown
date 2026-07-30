a7cf4bcefdc3eee7edf7e65509acb30cd9e66844
myown <parathranganathv@gmail.com>
2026-07-30 12:32:44 +0530

    Updated admin dashboard, calendar events, and Django admin configuration

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Budget, Expense, Earnings, Note
+from .models import Budget, Expense, Earnings, Note,CalendarEvent
 
 
 admin.site.register(Budget)
 admin.site.register(Expense)
 admin.site.register(Earnings)
 admin.site.register(Note)
+admin.site.register(CalendarEvent)
